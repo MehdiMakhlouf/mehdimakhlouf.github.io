0604158805687ff47ab74f85c11e83a1bfe50b8f
mehdi makhlouf <makhloufmehdi1@movinmotion.com>
2024-07-02 11:46:48 +0200

    chore(init): init app image compressor

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {HomePageComponentComponent} from "./home-page-component/home-page-component.component";
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomePageComponentComponent,
-  }
-];
